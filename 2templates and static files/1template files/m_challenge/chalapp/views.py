265bd5a1fbc969cd849d5949958ae6960674dec2
Sanish-Khadgi1137 <115631244+Sanish-Khadgi1137@users.noreply.github.com>
2025-06-18 10:15:25 +0545

    Update views.py

--- 2templates and static files/1template files/m_challenge/chalapp/views.py
+++ 2templates and static files/1template files/m_challenge/chalapp/views.py
@@ -45,7 +45,7 @@ def index(request):
 
     # return HttpResponse(resp_data)
 
-#tags : doing above logic using tags in index.html
+#tags : doing above logic using tags in index.html/ since this html file is inside app(chalapp) we do not need to add template DIR in setting.py but we have to register he app 
     return render(request, "chalapp/index2.html", {
          "mons" : months
     })
@@ -54,23 +54,24 @@ def index(request):
 def chal(request, month2):
     """Displaying clicked of the month"""
     try:
-        chal_text = monthly_challenges_dic[month2]
-                                         #app-name/file  app-name to avoid clashing of files with same name of different apps
+                                         #app-name/file  ;app-name to avoid clashing of files with same name of different apps
         # response_data = render_to_string("chalapp/challenges.html")#we need to configure/inform template folder or app to setting.py of project in "DIRS: []" or for app "INSTALLED_APPS = [
-
-        # return HttpResponse(response_data)
+        # return HttpResponse(response_data) #this just render challenges.html; displaying "Do work out for 22 min" for all months(click); nothing is passed
      
         #shorcuts; instead above 2 lines
         # return render(request, "chalapp/challenges.html")#request is necessary for render()
       
         #dynamic; for dynamic we use html + special DTL(django templated language) syntax eg "{{}}" in html file; commented {{}} is not allowed in normal-html file
+        chal_text = monthly_challenges_dic[month2]
+        
         return render(request, "chalapp/challenges2.html", {
             "text": chal_text,
             'month_name' : month2.capitalize()#do this inside html file using DTL for doing simple operation in html file
 
             })#we see hard coded text not place holder in html inspect because django analize and replaces those {{}} on the server and generate final html string or file and send to browser so browser only received that finish html hard coded string
 
-    except:
+    # except:
+    except keyError:#This ensures you only catch the case when the month is not in the dictionary.
         #return HttpResponseNotFound("<h1>This month is not supported!<h1>")
 
         #404 error with 404.html file
