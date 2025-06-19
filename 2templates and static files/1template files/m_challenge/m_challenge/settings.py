6496d6e5cf32de8b8e4294507347230a10be6ea9
Sanish-Khadgi1137 <115631244+Sanish-Khadgi1137@users.noreply.github.com>
2025-06-19 08:14:31 +0545

    Update settings.py

--- 2templates and static files/1template files/m_challenge/m_challenge/settings.py
+++ 2templates and static files/1template files/m_challenge/m_challenge/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+#for spefic css files; this is like registering app for template file inside app
 STATIC_URL = 'static/'  #3333333333needed to utilize static files
 
 # Default primary key field type
